ece39bfc1c87a1c048491e4b77bfb3c1044a8f1a
Courtney Yatteau <courtney.yatteau@gmail.com>
2020-10-31 14:40:45 -0400

    FAQs Setup

--- src/Components/Faq.js
+++ src/Components/Faq.js
@@ -1,56 +1,69 @@
 import React from 'react';
-import { Button, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
+import { Button, UncontrolledPopover, PopoverHeader, PopoverBody} from 'reactstrap';
 import Footer from './Footer';
-import { Accordion } from 'react-bootstrap';
+import { Accordion, Card } from 'react-bootstrap';
 
 const Faq = (props) => {
   return (
     <React.Fragment>
-      <div>
-        <br />
-        <Button className="btn" id="PopoverLegacy1" type="button" color="danger">When and where is this event?</Button>
-        <UncontrolledPopover trigger="legacy" placement="bottom" target="PopoverLegacy1">
-          <PopoverHeader>Location and Time</PopoverHeader>
-          <PopoverBody>
-          This event takes place in Fake City, Fake State at the Fake Plaza on Fake Street. The event will be on Saturday, FakeMonth 10th from noon-6pm. </PopoverBody>
-        </UncontrolledPopover>
-        {' '}
-        <br />
-        <br />
-        <Button className="btn" id="PopoverLegacy2" type="button" color="danger">What do I get for the price of admission?</Button>
-        <UncontrolledPopover trigger="focus" placement="bottom" target="PopoverLegacy2">
-          <PopoverHeader>Admission Perks</PopoverHeader>
-          <PopoverBody>Admission gets you entrance into our indulgent marketplace, where you will have access to various samples, foods, and beverages, plus you will be able to purchase a wonderful selection of products from our 50+ vendors.</PopoverBody>
-        </UncontrolledPopover>
-        {' '}
-        <br />
-        <br />
-        <Button className="btn" id="PopoverLegacy3" type="button" color="danger">Is there parking available at the event?</Button>
+      <Accordion>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="0">When and where is this event?</Accordion.Toggle>
+          </Card.Header>
+          
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>This event takes place in Fake City, Fake State at the Fake Plaza on Fake Street. The event will be on Saturday, FakeMonth 10th from noon-6pm.</Card.Body>
+          </Accordion.Collapse>
+
+        </Card>
+
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="1">What do I get for the price of admission?</Accordion.Toggle>
+          </Card.Header>
+
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Admission gets you entrance into our indulgent marketplace, where you will have access to various samples, foods, and beverages, plus you will be able to purchase a wonderful selection of products from our 50+ vendors.</Card.Body>
+          </Accordion.Collapse>
+
+        </Card>
+
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="2">Is there parking available at the event?</Accordion.Toggle>
+          </Card.Header>
+
+          <Accordion.Collapse eventKey="2">
+            <Card.Body>Yes! There are two parking garages withing 0.2 miles from Fake Plaza. One is just off of Faker Street and the other is just off of West Fake Drive.</Card.Body>
+          </Accordion.Collapse>
+
+        </Card>
+
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="3">Will tickets be sold at the door?</Accordion.Toggle>
+          </Card.Header>
+
+          <Accordion.Collapse eventKey="3">
+            <Card.Body>Yes, but they will be on a limited first come first serve basis. There is a limited amount of tickets sold day-of to make the experience pleasurable for all attendees. It's recommended you purchase tickets ahead of time online.</Card.Body>
+          </Accordion.Collapse>
+
+        </Card>
+
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="4">Are children welcome to this event?</Accordion.Toggle>
+          </Card.Header>
+
+          <Accordion.Collapse eventKey="4">
+            <Card.Body>Yes. Children under the age of 10 are free!</Card.Body>
+          </Accordion.Collapse>
+
+        </Card>
         
-        <UncontrolledPopover trigger="legacy" placement="bottom" target="PopoverLegacy3">
-          <PopoverHeader>Parking</PopoverHeader>
-          <PopoverBody>Yes! There are two parking garages withing 0.2 miles from Fake Plaza. One is just off of Faker Street and the other is just off of West Fake Drive.</PopoverBody>
-        </UncontrolledPopover>
-        {' '}
-        <br />
-        <br />
-        <Button className="btn" id="PopoverLegacy4" type="button" color="danger">Will tickets be sold at the door?</Button>
-
-        <UncontrolledPopover trigger="legacy" placement="bottom" target="PopoverLegacy4">
-          <PopoverHeader>Ticket Sales</PopoverHeader>
-          <PopoverBody>Yes, but they will be on a limited first come first serve basis. There is a limited amount of tickets sold day-of to make the experience pleasurable for all attendees. It's recommended you purchase tickets ahead of time online.</PopoverBody>
-        </UncontrolledPopover>
-        {' '}
-        <br />
-        <br />
-        <Button className="btn" id="PopoverLegacy5" type="button" color="danger">Are children welcome to this event?</Button>
-
-        <UncontrolledPopover trigger="legacy" placement="bottom" target="PopoverLegacy5">
-          <PopoverHeader>Children</PopoverHeader>
-          <PopoverBody>Yes. Children under the age of 10 are free!</PopoverBody>
-        </UncontrolledPopover>
-      </div>
-      <Footer />
+      </Accordion>
+            <Footer />
     </React.Fragment>
   );
 }
--- src/Components/Tickets.js
+++ src/Components/Tickets.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { UncontrolledCollapse } from 'reactstrap';
+import { Jumbotron, UncontrolledCollapse, Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 import Footer from './Footer';
 
 
@@ -15,17 +15,46 @@ class Tickets extends Component {
   render() {
     return (
       <React.Fragment>
-        <div>
-          <button id="test">Test</button>
-          <UncontrolledCollapse toggler={"#test"}>
-            Code here
-          </UncontrolledCollapse>
+        <Jumbotron>
+          <div>
+            <Card>
+              <CardTitle className="ticket-head">Ticket Options</CardTitle>
+            </Card>
+          </div>
+
           <br />
-          <button id="test2">Test</button>
-          <UncontrolledCollapse toggler={"#test2"}>
-            Code here
-          </UncontrolledCollapse>
-        </div>
+          <div className="row">
+            <div className="col-md-6">
+            <Card>
+              <CardTitle className="TicketsTitle">General Admission</CardTitle>
+              <CardBody>
+                <ul>
+                  <li>
+                     This ticket gives you access to the Indulgent Fest on either Saturday or Sunday starting at noon.
+                  </li>
+                </ul>
+              </CardBody>
+            </Card>
+            </div>
+            <div className="col-md-6">
+            <Card>
+              <CardTitle className="TicketsTitle">VIP Admission</CardTitle>
+              <ul>
+                <li>
+                  This ticket includes first access to the Indulgent Fest starting at 10am on either Saturday or Sunday.
+                </li>
+                <li>
+                  The special VIP hour from 10-11AM includes samples, cocktail sips, and a gift bag for shopping that is already filled with a few fun food treats.
+                </li>
+                <li>
+                  VIP is limited to 800 people in that exclusive hour, meaning shorter lines, smaller crowds, and more time for you and your friends to INDULGE!
+                </li>
+              </ul>
+              <CardBody></CardBody>
+            </Card>
+            </div>
+          </div>
+        </Jumbotron>
         <Footer />
       </React.Fragment>
     );
